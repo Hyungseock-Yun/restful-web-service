c26d87d77349db4daa9ef423513af1031938ad8c
Hyungseock-Yun <bagzein@gmail.com>
2022-07-01 01:20:03 +0900

    - 전체 회원 조회 - 특정 회원 조회 - 회원 등록

---
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -0,0 +1,39 @@
+package com.example.restfulwebservice.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+  private final UserDaoService service;
+
+  @GetMapping("/users")
+  public List<User> retrieveAllUsers() {
+    return service.findAll();
+  }
+
+  @GetMapping("/users/{id}")
+  public User retrieveUser(@PathVariable int id) {
+    return service.findOne(id);
+  }
+
+  @PostMapping("/users")
+  public ResponseEntity<User> createUser(@RequestBody User user) {
+    User savedUser = service.save(user);
+
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+      .path("/{id}")
+      .buildAndExpand(savedUser.getId())
+      .toUri();
+
+    return ResponseEntity.created(location).build();
+  }
+
+}
