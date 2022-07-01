c3480980fd170fbb931668ddb6726c6cd72e5e71
Hyungseock-Yun <bagzein@gmail.com>
2022-07-02 02:07:47 +0900

    - Http Status code를 이용한 예외 처리

---
+++ src/main/java/com/example/restfulwebservice/exception/ExceptionResponse.java
@@ -0,0 +1,20 @@
+package com.example.restfulwebservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponse {
+
+  private LocalDateTime timestamp;
+
+  private String msg;
+
+  private String details;
+
+}
