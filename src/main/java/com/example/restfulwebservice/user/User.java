6d856ff9bf84d90cac2302499749a3916d780db7
Hyungseock-Yun <bagzein@gmail.com>
2022-07-11 23:19:10 +0900

    - admin Controller 및 filtering 방법

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,5 +1,7 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +11,8 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+//@JsonIgnoreProperties(value = {"password"})
+@JsonFilter("UserInfo")
 public class User {
 
   private Integer id;
@@ -18,5 +22,9 @@ public class User {
 
   @Past
   private Date joinDate;
+
+  private String password;
+
+  private String ssn;
 
 }
